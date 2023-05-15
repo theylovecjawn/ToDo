15f8875b5ea49cdd7fbc27d8eaec838f150bc1a1
theylovecjawn <calebjohn234@gmail.com>
2023-05-15 14:46:17 -0400

    Todo almost done #2

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
-  import {ref} from 'vue'
+  import {ref, watch} from 'vue'
 
-  let todos = ref([])
+  let todos = ref(JSON.parse(window.localStorage.getItem('todos')))
+
+
+
+  watch(todos, function(value){
+    window.localStorage.setItem('todos', JSON.stringify(value))
+  }, {deep: true})
+  
   let newTodo = ref("")
+
 
   function popUpTodo(){
     todos.value.push({
@@ -22,7 +30,7 @@
   <li v-for="(todo, index) in todos">
     <button id="icon" @click="deleteTodo(index)">🗑</button>
   {{ todo.text }}
-  <input type="checkbox" v-model="todo.complete">
+  <label class="container"><input type="checkbox" v-model="todo.complete"><span class="checkmark"></span></label>
   </li>
 </ol>
 <p><input v-model="newTodo" @keydown.enter="popUpTodo" id="inputBox" placeholder="Click Me to Start"></p>
@@ -75,5 +83,73 @@ p{
   border-radius: 8px;
 
 }
+/* Customize the label (the container) */
+.container {
+  margin: 20px;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 15px;
+  cursor: pointer;
+  font-size: 22px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.container input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: #eee;
+}
+
+/* On mouse-over, add a grey background color */
+.container:hover input ~ .checkmark {
+  background-color: #ccc;
+}
+
+/* When the checkbox is checked, add a blue background */
+.container input:checked ~ .checkmark {
+  background-color: #2196F3;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.container input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the checkmark/indicator */
+.container .checkmark:after {
+  left: 9px;
+  top: 5px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+
 
 </style>
